a11ea48b9097c54699cfd31a3029cd37ec53108b
Bi1ku <2008owenshi@gmail.com>
2025-10-04 23:58:58 -0400

    limelight code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -7,9 +7,13 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.utils.commands.PathCommand;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -28,13 +32,22 @@ public class Drivetrain extends SubsystemBase {
     public static boolean ROBOT_CENTRIC = true;
     private final Follower follower;
 
+    private final Limelight3A limelight;
+    private final GamepadEx gamepad;
+
     private double xPower = 0.0;
     private double angPower = 0.0;
     private double yPower = 0.0;
 
-    public Drivetrain(final HardwareMap hwMap, Pose startPose) {
+    public Drivetrain(final HardwareMap hwMap, Pose startPose, GamepadEx gamepad) {
         this.follower = Constants.createFollower(hwMap);
         this.follower.setStartingPose(startPose);
+
+        this.limelight = hwMap.get(Limelight3A.class, "limelight");
+        this.limelight.pipelineSwitch(0);
+        this.limelight.start();
+
+        this.gamepad = gamepad;
     }
 
     private static double calculateAccel(double accel, double deaccel, double prevPower, double check) {
@@ -47,27 +60,58 @@ public class Drivetrain extends SubsystemBase {
         return check - prevPower >= 0 ? rel : -rel;
     }
 
-    public void startThread(final GamepadEx gamepad, CommandOpMode opMode) {
-        new Thread(() -> {
-            this.follower.startTeleopDrive();
+//    public void startThread(final GamepadEx gamepad, CommandOpMode opMode) {
+//        new Thread(() -> {
+//            this.follower.startTeleopDrive();
+//
+//            while (opMode.opModeIsActive())
+//                try {
+//                    synchronized (this.follower) {
+//                        this.yPower += Drivetrain.calculateAccel(MAX_ACCEL, MAX_DEACCEL, this.yPower, gamepad.getLeftY());
+//                        this.xPower += Drivetrain.calculateAccel(MAX_ACCEL, MAX_DEACCEL, this.xPower, -gamepad.getLeftX());
+//                        this.angPower += Drivetrain.calculateAccel(MAX_ANGULAR_ACCEL, MAX_ANGULAR_DEACCEL, this.angPower, gamepad.getRightX());
+//
+//                        this.follower.setTeleOpDrive(yPower * MAX_VEL, xPower * MAX_VEL, angPower * MAX_ANGULAR_VEL, ROBOT_CENTRIC);
+//                        this.follower.update();
+//                    }
+//                    Thread.sleep(10);
+//                } catch (InterruptedException e) {
+//                    StringWriter errors = new StringWriter();
+//                    e.printStackTrace(new PrintWriter(errors));
+//                    PanelsTelemetry.INSTANCE.getTelemetry().addLine(errors.toString());
+//                }
+//        }).start();
+//    }
 
-            while (opMode.opModeIsActive())
-                try {
-                    synchronized (this.follower) {
-                        this.yPower += Drivetrain.calculateAccel(MAX_ACCEL, MAX_DEACCEL, this.yPower, gamepad.getLeftY());
-                        this.xPower += Drivetrain.calculateAccel(MAX_ACCEL, MAX_DEACCEL, this.xPower, -gamepad.getLeftX());
-                        this.angPower += Drivetrain.calculateAccel(MAX_ANGULAR_ACCEL, MAX_ANGULAR_DEACCEL, this.angPower, gamepad.getRightX());
+    @Override
+    public void periodic() {
+        this.yPower += Drivetrain.calculateAccel(MAX_ACCEL, MAX_DEACCEL, this.yPower, gamepad.getLeftY());
+        this.xPower += Drivetrain.calculateAccel(MAX_ACCEL, MAX_DEACCEL, this.xPower, -gamepad.getLeftX());
+        this.angPower += Drivetrain.calculateAccel(MAX_ANGULAR_ACCEL, MAX_ANGULAR_DEACCEL, this.angPower, gamepad.getRightX());
 
-                        this.follower.setTeleOpDrive(yPower * MAX_VEL, xPower * MAX_VEL, angPower * MAX_ANGULAR_VEL, ROBOT_CENTRIC);
-                        this.follower.update();
-                    }
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    StringWriter errors = new StringWriter();
-                    e.printStackTrace(new PrintWriter(errors));
-                    PanelsTelemetry.INSTANCE.getTelemetry().addLine(errors.toString());
-                }
-        }).start();
+        this.follower.setTeleOpDrive(yPower * MAX_VEL, xPower * MAX_VEL, angPower * MAX_ANGULAR_VEL, ROBOT_CENTRIC);
+        this.follower.update();
+    }
+
+    public void alignToShoot() {
+        LLResult llResult = limelight.getLatestResult();
+        if (llResult != null && llResult.isValid()) {
+            Pose3D botpose = llResult.getBotpose();
+
+            PanelsTelemetry.INSTANCE.getTelemetry().addData("Target X", llResult.getTx());
+            PanelsTelemetry.INSTANCE.getTelemetry().addData("Target Y", llResult.getTy());
+            PanelsTelemetry.INSTANCE.getTelemetry().addData("Target Area", llResult.getTa());
+            PanelsTelemetry.INSTANCE.getTelemetry().addData("Bot Pose", botpose.toString());
+            PanelsTelemetry.INSTANCE.getTelemetry().addData("Yaw", botpose.getOrientation().getYaw());
+
+            // using the provided data from the lightlight, align the robot so that it is facing the goal
+            // you can use pedropathing's follower to do this i.e., calculate the correct point we want to go to.
+            // double tx = result.getTx(); // How far left or right the target is (degrees)
+            // double ty = result.getTy(); // How far up or down the target is (degrees)
+            // double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+
+            // new PathCommand(buildLine(currentPose => new Pose)).schedule();
+        }
     }
 
     public Follower getFollower() {

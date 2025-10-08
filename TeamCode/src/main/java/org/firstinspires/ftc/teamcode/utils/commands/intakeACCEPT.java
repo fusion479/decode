8af958a4c3639169dbbb72a588260a2f81be4853
Cassandra Chan <cassandrachan@cassandras-air.lan>
2025-10-07 21:24:21 -0400

    intakeACCEPT command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/commands/intakeACCEPT.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.utils.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+
+public class intakeACCEPT extends CommandBase {
+    private final Intake intake;
+
+    public intakeACCEPT(final Intake intake) {
+        this.intake = intake;
+        super.addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+        this.intake.setPower(Intake.ACCEPT);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}

50460d268b6ee67a3b7af78a8579452f08400323
Cassandra Chan <cassandrachan0137@gmail.com>
2025-10-27 19:48:30 +0000

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/bottom6red.pp
@@ -0,0 +1,105 @@
+{
+    "startPoint": {
+        "x": 88,
+        "y": 8.2,
+        "heading": "linear",
+        "startDeg": 90,
+        "endDeg": 180,
+        "index": 0
+    },
+    "lines": [
+        {
+            "name": "Path 1",
+            "endPoint": {
+                "x": 72.1,
+                "y": 22.7,
+                "heading": "linear",
+                "startDeg": 90,
+                "endDeg": 135,
+                "index": 1
+            },
+            "controlPoints": [],
+            "color": "#555DCD"
+        },
+        {
+            "name": "Path 2",
+            "endPoint": {
+                "x": 43,
+                "y": 35.5,
+                "heading": "linear",
+                "reverse": false,
+                "startDeg": 135,
+                "endDeg": 180,
+                "index": 2
+            },
+            "controlPoints": [],
+            "color": "#CABC7C"
+        },
+        {
+            "name": "Path 3",
+            "endPoint": {
+                "x": 22,
+                "y": 35.5,
+                "heading": "tangential",
+                "reverse": false,
+                "index": 3
+            },
+            "controlPoints": [],
+            "color": "#97D9DC"
+        },
+        {
+            "name": "Path 4",
+            "endPoint": {
+                "x": 72.1,
+                "y": 22.7,
+                "heading": "linear",
+                "reverse": false,
+                "startDeg": 180,
+                "endDeg": 135,
+                "index": 4
+            },
+            "controlPoints": [],
+            "color": "#A88B75"
+        },
+        {
+            "name": "Path 5",
+            "endPoint": {
+                "x": 43,
+                "y": 60,
+                "heading": "linear",
+                "reverse": false,
+                "startDeg": 135,
+                "endDeg": 180,
+                "index": 5
+            },
+            "controlPoints": [],
+            "color": "#989A69"
+        },
+        {
+            "name": "Path 6",
+            "endPoint": {
+                "x": 22,
+                "y": 60,
+                "heading": "tangential",
+                "reverse": false,
+                "index": 6
+            },
+            "controlPoints": [],
+            "color": "#CCDCD7"
+        },
+        {
+            "name": "Path 7",
+            "endPoint": {
+                "x": 72.1,
+                "y": 22.7,
+                "heading": "linear",
+                "reverse": false,
+                "startDeg": 180,
+                "endDeg": 135,
+                "index": 7
+            },
+            "controlPoints": [],
+            "color": "#6B699A"
+        }
+    ]
+}

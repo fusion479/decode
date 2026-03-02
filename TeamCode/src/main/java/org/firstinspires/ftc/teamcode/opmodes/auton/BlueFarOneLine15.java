52a4818bda342147c5c7ec9374428790b9aa6af9
Stephen Chen <stephen.chen420@gmail.com>
2026-03-02 18:37:52 -0500

    3/2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/BlueFarOneLine15.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/BlueFarOneLine15.java
@@ -1,4 +1,127 @@
 package org.firstinspires.ftc.teamcode.opmodes.auton;
 
-public class BlueFarOneLine15 {
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.CommandRobot;
+import org.firstinspires.ftc.teamcode.commands.transfer.TransferAccept;
+import org.firstinspires.ftc.teamcode.opmodes.auton.trajectories.BlueFarOneLine15Trajectories;
+import org.firstinspires.ftc.teamcode.opmodes.auton.trajectories.BlueFarOneLineTrajectories;
+import org.firstinspires.ftc.teamcode.utils.commands.OpModeCore;
+import org.firstinspires.ftc.teamcode.utils.commands.PathCommand;
+
+@Configurable
+@Autonomous(name = "Blue Far One Line 15", preselectTeleOp = "BlueMain")
+public class BlueFarOneLine15 extends OpModeCore {
+    public static double SCORE_SPEED = 1;
+    public static double NORMAL_SPEED = 1;
+
+    public static double HP_SPEED = 0.7;
+    public static double INTAKE_SPEED = 1;
+    public static int SHOOT_DURATION = 1500;
+    public static int INTAKE_DURATION = 1700;
+    public static int INTAKE_DURATION_HP = 2500;
+    public static int SHOOT_WAIT = 100;
+
+    public static int FIRST_SHOOT_WAIT = 1100;
+
+    public static int INTAKE_HP_WAIT = 0;
+
+    private CommandRobot robot;
+    private BlueFarOneLine15Trajectories trajectories;
+
+    @Override
+    public void initialize() {
+        this.trajectories = new BlueFarOneLine15Trajectories();
+
+        this.robot = new CommandRobot(super.hardwareMap, super.gamepad1, this.trajectories.getStart(), "blue");
+    }
+
+    @Override
+    public void runOpMode() {
+        CommandScheduler.getInstance().enable();
+        this.initialize();
+
+        super.waitForStart();
+
+        CommandScheduler.getInstance().schedule(
+                new SequentialCommandGroup(
+                        new ParallelCommandGroup(
+                                new PathCommand(this.robot, this.trajectories.shootFirst, SCORE_SPEED),
+                                robot.autonFar()
+                        ),
+                        new WaitCommand(FIRST_SHOOT_WAIT),
+
+                        robot.shoot(),
+                        new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), SHOOT_DURATION),
+                        robot.ready(),
+
+                        new ParallelCommandGroup(
+                                new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), INTAKE_DURATION_HP),
+                                new SequentialCommandGroup(
+                                        new PathCommand(this.robot, this.trajectories.intakeSecond, HP_SPEED),
+                                        new WaitCommand(INTAKE_HP_WAIT)
+                                )
+                        ),
+
+                        new PathCommand(this.robot, this.trajectories.shootSecond, SCORE_SPEED),
+                        new WaitCommand(SHOOT_WAIT),
+                        robot.shoot(),
+                        new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), SHOOT_DURATION),
+                        robot.ready(),
+
+                        new PathCommand(this.robot, this.trajectories.setupThird, NORMAL_SPEED),
+                        new ParallelCommandGroup(
+                                new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), INTAKE_DURATION),
+                                new PathCommand(this.robot, this.trajectories.intakeThird, INTAKE_SPEED)
+                        ),
+
+                        new PathCommand(this.robot, this.trajectories.shootThird, SCORE_SPEED),
+                        new WaitCommand(SHOOT_WAIT),
+                        robot.shoot(),
+                        new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), SHOOT_DURATION),
+                        robot.ready(),
+
+                        new ParallelCommandGroup(
+                                new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), INTAKE_DURATION_HP),
+                                new SequentialCommandGroup(
+                                        new PathCommand(this.robot, this.trajectories.intakeFourth, INTAKE_SPEED),
+                                        new WaitCommand(INTAKE_HP_WAIT)
+                                )
+                        ),
+
+                        new PathCommand(this.robot, this.trajectories.shootFourth, SCORE_SPEED),
+                        new WaitCommand(SHOOT_WAIT),
+                        robot.shoot(),
+                        new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), SHOOT_DURATION),
+                        robot.ready(),
+
+                        new ParallelCommandGroup(
+                                new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), INTAKE_DURATION_HP),
+                                new SequentialCommandGroup(
+                                        new PathCommand(this.robot, this.trajectories.intakeFifth, INTAKE_SPEED),
+                                        new WaitCommand(INTAKE_HP_WAIT)
+                                )
+                        ),
+
+                        new PathCommand(this.robot, this.trajectories.shootFifth, SCORE_SPEED),
+                        new WaitCommand(SHOOT_WAIT),
+                        robot.shoot(),
+                        new TransferAccept(this.robot.getIntake(), this.robot.getTransfer(), SHOOT_DURATION),
+                        robot.ready(),
+
+                        new PathCommand(this.robot, this.trajectories.park, 1)
+                )
+        );
+
+        while (opModeIsActive()) {
+            CommandScheduler.getInstance().run();
+        }
+
+        super.end();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/trajectories/positions/BlueFarOneLine15.pp
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/trajectories/positions/BlueFarOneLine15.pp
@@ -1,146 +1,176 @@
 {
-    "startPoint": {
-        "x": 56,
-        "y": 8.2,
+  "startPoint": {
+    "x": 56,
+    "y": 8.2,
+    "heading": "linear",
+    "startDeg": 180,
+    "endDeg": 180,
+    "index": 0
+  },
+  "lines": [
+    {
+      "name": "shootFirst",
+      "endPoint": {
+        "x": 55,
+        "y": 12.5,
         "heading": "linear",
         "startDeg": 180,
-        "endDeg": 180,
-        "index": 0
+        "endDeg": 199,
+        "name": "shootFirst",
+        "index": 1
+      },
+      "controlPoints": [],
+      "color": "#C5AD6D"
     },
-    "lines": [
-        {
-            "name": "shootFirst",
-            "endPoint": {
-                "x": 55,
-                "y": 12.5,
-                "heading": "linear",
-                "startDeg": 180,
-                "endDeg": 205,
-                "name": "shootFirst",
-                "index": 1
-            },
-            "controlPoints": [],
-            "color": "#C5AD6D"
-        },
-        {
-            "name": "intakeHP",
-            "endPoint": {
-                "x": 12,
-                "y": 10.5,
-                "heading": "linear",
-                "reverse": false,
-                "name": "intakeHP",
-                "startDeg": 205,
-                "endDeg": 180,
-                "index": 2
-            },
-            "controlPoints": [],
-            "color": "#59DA8D"
-        },
-        {
-            "name": "shootSecond",
-            "endPoint": {
-                "x": 50,
-                "y": 14,
-                "heading": "linear",
-                "reverse": false,
-                "name": "shootSecond",
-                "startDeg": 180,
-                "endDeg": 195,
-                "index": 3
-            },
-            "controlPoints": [],
-            "color": "#556695"
-        },
-        {
-            "name": "setupThird",
-            "endPoint": {
-                "x": 44,
-                "y": 34,
-                "heading": "linear",
-                "reverse": false,
-                "name": "setupThird",
-                "startDeg": 195,
-                "endDeg": 180,
-                "index": 4
-            },
-            "controlPoints": [],
-            "color": "#6CDA7C"
-        },
-        {
-            "name": "intakeThird",
-            "endPoint": {
-                "x": 14,
-                "y": 34,
-                "heading": "linear",
-                "reverse": false,
-                "name": "intakeThird",
-                "startDeg": 180,
-                "endDeg": 180,
-                "index": 5
-            },
-            "controlPoints": [],
-            "color": "#ABC6D8"
-        },
-        {
-            "name": "shootThird",
-            "endPoint": {
-                "x": 53,
-                "y": 14,
-                "heading": "linear",
-                "reverse": false,
-                "name": "shootThird",
-                "startDeg": 180,
-                "endDeg": 200,
-                "index": 6
-            },
-            "controlPoints": [],
-            "color": "#A577BD"
-        },
-        {
-            "name": "intakeFourth",
-            "endPoint": {
-                "x": 14,
-                "y": 16,
-                "heading": "linear",
-                "reverse": false,
-                "name": "intakeThird",
-                "startDeg": 200,
-                "endDeg": 180,
-                "index": 5
-            },
-            "controlPoints": [],
-            "color": "#ABC6D8"
-        },
-        {
-            "name": "shootFourth",
-            "endPoint": {
-                "x": 51,
-                "y": 14,
-                "heading": "linear",
-                "reverse": false,
-                "name": "shootThird",
-                "startDeg": 180,
-                "endDeg": 197,
-                "index": 6
-            },
-            "controlPoints": [],
-            "color": "#A577BD"
-        },
-        {
-            "name": "park",
-            "endPoint": {
-                "x": 54,
-                "y": 40,
-                "heading": "linear",
-                "reverse": false,
-                "name": "park",
-                "startDeg": 197,
-                "endDeg": 180,
-                "index": 7
-            },
-            "controlPoints": [],
-            "color": "#88ACB5"
-        }
-    ]
+    {
+      "name": "intakeHP",
+      "endPoint": {
+        "x": 16,
+        "y": 8.5,
+        "heading": "linear",
+        "reverse": false,
+        "name": "intakeHP",
+        "startDeg": 199,
+        "endDeg": 180,
+        "index": 2
+      },
+      "controlPoints": [],
+      "color": "#59DA8D"
+    },
+    {
+      "name": "shootSecond",
+      "endPoint": {
+        "x": 46,
+        "y": 14,
+        "heading": "linear",
+        "reverse": false,
+        "name": "shootSecond",
+        "startDeg": 180,
+        "endDeg": 188,
+        "index": 3
+      },
+      "controlPoints": [],
+      "color": "#556695"
+    },
+    {
+      "name": "setupThird",
+      "endPoint": {
+        "x": 44,
+        "y": 34,
+        "heading": "linear",
+        "reverse": false,
+        "name": "setupThird",
+        "startDeg": 188,
+        "endDeg": 180,
+        "index": 4
+      },
+      "controlPoints": [],
+      "color": "#6CDA7C"
+    },
+    {
+      "name": "intakeThird",
+      "endPoint": {
+        "x": 14,
+        "y": 34,
+        "heading": "linear",
+        "reverse": false,
+        "name": "intakeThird",
+        "startDeg": 180,
+        "endDeg": 180,
+        "index": 5
+      },
+      "controlPoints": [],
+      "color": "#ABC6D8"
+    },
+    {
+      "name": "shootThird",
+      "endPoint": {
+        "x": 48,
+        "y": 16,
+        "heading": "linear",
+        "reverse": false,
+        "name": "shootThird",
+        "startDeg": 180,
+        "endDeg": 192,
+        "index": 6
+      },
+      "controlPoints": [],
+      "color": "#A577BD"
+    },
+    {
+      "name": "intakeFourth",
+      "endPoint": {
+        "x": 18,
+        "y": 16,
+        "heading": "linear",
+        "reverse": false,
+        "name": "intakeFourth",
+        "startDeg": 192,
+        "endDeg": 180,
+        "index": 5
+      },
+      "controlPoints": [],
+      "color": "#ABC6D8"
+    },
+    {
+      "name": "shootFourth",
+      "endPoint": {
+        "x": 46,
+        "y": 14,
+        "heading": "linear",
+        "reverse": false,
+        "name": "shootFourth",
+        "startDeg": 180,
+        "endDeg": 189,
+        "index": 6
+      },
+      "controlPoints": [],
+      "color": "#A577BD"
+    },
+    {
+      "name": "intakeFifth",
+      "endPoint": {
+        "x": 18,
+        "y": 16,
+        "heading": "linear",
+        "reverse": false,
+        "name": "intakeFifth",
+        "startDeg": 189,
+        "endDeg": 180,
+        "index": 5
+      },
+      "controlPoints": [],
+      "color": "#ABC6D8"
+    },
+    {
+      "name": "shootFifth",
+      "endPoint": {
+        "x": 46,
+        "y": 14,
+        "heading": "linear",
+        "reverse": false,
+        "name": "shootFifth",
+        "startDeg": 180,
+        "endDeg": 188,
+        "index": 6
+      },
+      "controlPoints": [],
+      "color": "#A577BD"
+    },
+    {
+      "name": "park",
+      "endPoint": {
+        "x": 54,
+        "y": 40,
+        "heading": "linear",
+        "reverse": false,
+        "name": "park",
+        "startDeg": 188,
+        "endDeg": 180,
+        "index": 7
+      },
+      "controlPoints": [],
+      "color": "#88ACB5"
+    }
+  ]
 }

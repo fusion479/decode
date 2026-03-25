fe99b1a803aa4f460c72c59b40f3b1c64d4fe2bd
Cassandra Chan <cassandrachan@Cassandras-MacBook-Air.local>
2026-03-25 16:02:15 -0400

    redcloseramp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/trajectories/positions/RedCloseRamp12.pp
@@ -0,0 +1,210 @@
+{
+  "startPoint": {
+    "x": 111,
+    "y": 136,
+    "heading": "linear",
+    "startDeg": 90,
+    "endDeg": 180,
+    "locked": false
+  },
+  "lines": [
+    {
+      "id": "line-mczqq4xyed",
+      "name": "shootFirst",
+      "endPoint": {
+        "x": 92,
+        "y": 91.5,
+        "heading": "linear",
+        "startDeg": 90,
+        "endDeg": 136
+      },
+      "controlPoints": [],
+      "color": "#D67B88",
+      "locked": false,
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6gakb6-wvxz0m",
+      "name": "setupSecond",
+      "endPoint": {
+        "x": 95,
+        "y": 60,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 136,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#58BAB6",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6gdouh-nbodks",
+      "name": "intakeSecond",
+      "endPoint": {
+        "x": 130,
+        "y": 60,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#DCA76C",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6ge3rq-kwuxui",
+      "name": "shootSecond",
+      "endPoint": {
+        "x": 92,
+        "y": 91.5,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 136
+      },
+      "controlPoints": [],
+      "color": "#7DA79D",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6gffoe-wv3s9b",
+      "name": "setupThird",
+      "endPoint": {
+        "x": 115,
+        "y": 66,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 136,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#BA965D",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6ggwv9-4re09b",
+      "name": "intakeThird",
+      "endPoint": {
+        "x": 133,
+        "y": 62,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 210
+      },
+      "controlPoints": [],
+      "color": "#9957A7",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6gprpa-2hdv8r",
+      "name": "shootThird",
+      "endPoint": {
+        "x": 92,
+        "y": 91.5,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 210,
+        "endDeg": 136
+      },
+      "controlPoints": [],
+      "color": "#75C959",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6gqptn-qwqwbe",
+      "name": "setupFourth",
+      "endPoint": {
+        "x": 95,
+        "y": 85,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 136,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#C699AD",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6gro8k-11ly0k",
+      "name": "intakeFourth",
+      "endPoint": {
+        "x": 125,
+        "y": 85,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#C9B7B6",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6gsdyb-gjtuo0",
+      "name": "shootFourth",
+      "endPoint": {
+        "x": 92,
+        "y": 91.5,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 136
+      },
+      "controlPoints": [],
+      "color": "#97A985",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mn6gsv4y-3g7iux",
+      "name": "park",
+      "endPoint": {
+        "x": 95,
+        "y": 70,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 136,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#CC7CD7",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ]
+}
